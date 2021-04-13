f887c38975cfc5b04bc0e40796fde4ed2187c910
Mikhail <s_m86@mail.ru>
2021-04-13 23:02:12 +0300

    start2

---
+++ HW2.7.1/DataManager/DataPersons.swift
@@ -0,0 +1,52 @@
+//
+//  DataPersons.swift
+//  HW2.7.1
+//
+//  Created by михаил сулим on 13.04.2021.
+//
+
+import Foundation
+
+
+struct DataPersons {
+    var firstNames = ["Jack",
+                      "Pol",
+                      "Nick",
+                      "Jone",
+                      "Alex",
+                      "Adam",
+                      "Albert",
+                      "Carl",
+                      "Cory",
+                      "David"]
+    var lastNames = ["Brooke",
+                     "Brown",
+                     "Cole",
+                     "Ford",
+                     "Hail",
+                     "Hunt",
+                     "Joy",
+                     "Love",
+                     "Ray",
+                     "Scott"]
+    var phoneNumbers = ["23452344",
+                        "93934939",
+                        "39605949",
+                        "86939585",
+                        "71727172",
+                        "90101010",
+                        "09376850",
+                        "03885837",
+                        "09609404",
+                        "94838000"]
+    var emails = ["sdfsdfsd@mail.com",
+                  "lolofdd@mail.com",
+                  "wewewew@mail.com",
+                  "kikikik@mail.com",
+                  "popopo@mail.com",
+                  "wywywyywy@mail.com",
+                  "lilili@mail.com",
+                  "seseses@mail.com",
+                  "mimimi@mail.com",
+                  "qoqoqo@mail.com"]
+}
---
+++ HW2.7.1/Model/Persons.swift
@@ -0,0 +1,8 @@
+//
+//  Persons.swift
+//  HW2.7.1
+//
+//  Created by михаил сулим on 13.04.2021.
+//
+
+import Foundation
